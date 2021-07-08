93cfba60bf44e409c92364a49b1752fe30502104
soup-bowl <code@soupbowl.io>
2021-07-09 00:03:10 +0100

    Register footer menu.

--- functions.php
+++ functions.php
@@ -14,3 +14,25 @@ add_action(
 		wp_enqueue_script( 'rt-dark', get_stylesheet_directory_uri() . '/dark.js', array(), '1', true );
 	}
 );
+
+// Create additional menus.
+add_action(
+	'after_setup_theme',
+	function() {
+		register_nav_menus([
+			'footer_menu' => 'Footer Menu',
+		]);
+	}
+);
+
+// Display menu in the footer part.
+add_action(
+	'generate_footer',
+	function() {
+		wp_nav_menu([
+			'fallback_cb' => false,
+			'depth'       => 1,
+			'menu'        => 'footer_menu',
+		]);
+	}
+);
